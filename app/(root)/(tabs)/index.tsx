d3c6e77b320444d90c2e880d7de0f6a8f337e3ce
kiptoo <brianrtt@gmail.com>
2025-03-05 20:58:54 +0300

    updates

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to ReEstate</Text>
+      <Text className="font-bold  my-10 font-rubik text-3xl">Welcome to ReEstate</Text>
      <Link href="/sign-in">Sign in </Link>
      <Link href="/explore">Explore</Link>
      <Link href="/properties/[id]">Property</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
 import "./global.css"
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -26,5 +27,9 @@ export default function RootLayout() {
   if (!fontsLoaded) {
     return null;
   }
-  return <Stack />;
+  return(
+    <GlobalProvider>
+     <Stack screenOptions={{headerShown:false}} />;
+    </GlobalProvider>
+  ) 
 }
