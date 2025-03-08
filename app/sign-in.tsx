56845d06a7e432b620bbd58b3a89fe379099b064
kiptoo <brianrtt@gmail.com>
2025-03-08 11:03:27 +0300

    updates

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView,Image, TouchableOpacity, Alert } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
@@ -6,18 +6,18 @@ import icons from '@/constants/icons'
 import { login } from '@/lib/appwrite'
 
 const SignIn = () => {
-  const handleLogin=async()=>{
-    const result=await login();
-    if(result){
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
       console.log("Login success");
-    }else{
-      Alert.alert('Error','Failed to login')
+    } else {
+      Alert.alert('Error', 'Failed to login')
     }
   }
   return (
     <SafeAreaView className='bg-white h-full'>
-      <ScrollView contentContainerClassName='h-full'>
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'></Image>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
       </ScrollView>
       <View className='px-10'>
         <Text className='text-base text-center uppercase font-rubik text-black-200'>
@@ -32,12 +32,9 @@ const SignIn = () => {
         </Text>
         <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
           <View className='flex flex-row items-center justify-center'>
-          <Image source={icons.google} className='w-5 h-5'resizeMode='contain'/>
-          <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+            <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
           </View>
-          
-
-        
         </TouchableOpacity>
       </View>
     </SafeAreaView>
